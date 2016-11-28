d86195560c60f0ef7c8d993e719daa6d0ca31186
chriscauley <chris@lablackey.com>
2016-11-28 12:43:23 -0500

    burning out modal.tag with dialog.js and mask.less

---
+++ dialog.js
@@ -0,0 +1,16 @@
+var DialogMixin = {
+  init: function() {
+    if (this.opts.ur_modal){
+      this.theme = this.opts.theme || uR.theme.modal;
+      var e = document.createElement('div');
+      e.setAttribute("ur-mask",true);
+      if (this.root.childNodes.length) {
+        this.root.insertBefore(e,this.root.childNodes[0])
+      } else {
+        this.root.appendChild(e);
+      }
+    }
+  }
+}
+
+riot.mixin(DialogMixin)
