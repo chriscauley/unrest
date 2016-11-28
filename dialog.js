d1cbd2d6f02e92aea42c92f5e8a6801124add575
chriscauley <chris@lablackey.com>
2016-11-28 14:39:35 -0500

    bind dialog with unmount on click

--- dialog.js
+++ dialog.js
@@ -3,6 +3,7 @@ var DialogMixin = {
     if (this.opts.ur_modal){
       this.theme = this.opts.theme || uR.theme.modal;
       var e = document.createElement('div');
+      e.addEventListener("click",(function() { this.unmount() }).bind(this));
       e.setAttribute("ur-mask",true);
       if (this.root.childNodes.length) {
         this.root.insertBefore(e,this.root.childNodes[0])
