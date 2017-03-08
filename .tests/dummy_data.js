eacdf0301186e7e7f9350f48495c2c8c795f8c06
chriscauley <chris@lablackey.com>
2017-03-08 15:13:58 -0500

    using new uR.TESTING parameter

--- .tests/dummy_data.js
+++ .tests/dummy_data.js
@@ -3,12 +3,14 @@ var SCHEMA = [
   {name: "email", type: "email"},
   {name: 'select_color', type: 'select', choices: ['red','green','blue']},
   {name: "textarea", type: "textarea"},
-  {name: "checkboxes", type: "checkbox-input",choices: ['red','green','blue']},
-  {name: "radioes", type: "radio",choices: ['red','green','blue']},
+  {name: "checkboxes", type: "checkbox",choices: ['red','green','blue']},
+  {name: "radios", type: "radio",choices: ['red','green','blue']},
 ];
 var INITIAL = {
   first_name: "bob",
   email: "arst@arst.com",
   select_color: 'red',
   textarea: "this is a story all about how my life got flipped, turned up side down.",
+  radios: "blue",
+  checkboxes: "green",
 };
