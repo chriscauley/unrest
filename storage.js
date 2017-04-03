a8405e2d3289c1c747a3598b52c0a2a6a70082ca
chriscauley <chris@lablackey.com>
2017-04-03 02:08:21 -0400

    adding remove and a pseudo file system

--- storage.js
+++ storage.js
@@ -1,5 +1,4 @@
-uR = window.uR || {};
-uR.storage = (function() {
+(function() {
   // basic functions
   function get(key) {
     var value;
@@ -14,17 +13,25 @@ uR.storage = (function() {
   function set(key,value) {
     if (!value && value !== 0) { localStorage.removeItem(key); return; }
     localStorage.setItem(key,JSON.stringify(value))
+    uR.storage.times[key] = new Date().valueOf();
+    localStorage.setItem('times',JSON.stringify(uR.storage.times));
   }
   function has(key) { return localStorage.hasOwnProperty(key) }
+
+  function remove(key) {
+    localStorage.removeItem(key);
+    delete uR.storage.times[key];
+    localStorage.setItem('times',JSON.stringify(uR.storage.times));
+  }
 
   try {
     localStorage.setItem('test', '1');
     localStorage.removeItem('test');
-    if (uR.getQueryParameter("FAKE_STORAGE") != -1) { throw "Faking storage" }
   } catch(e) {
     function set(key,value) { uR.storage.FAKE_STORAGE[key] = value }
     function get(key) { return uR.storage.FAKE_STORAGE[key]; }
     function has(key) { uR.storage.FAKE_STORAGE.hasOwnProperty(key); }
+    function remove(key) { delete uR.storage.FAKE_STORAGE[key]; }
   }
 
   // timebomb remote data store
@@ -51,14 +58,16 @@ uR.storage = (function() {
     });
   }
 
-  return {
+  uR.storage = {
     get: get,
     set: set,
     has: has,
+    remove: remove,
     FAKE_STORAGE: {},
     lookups: {}, // functions to look up missing/expired values
     defaults: {}, // table with default values
     remote: remote,
     default_expire_ms: 60*1000, // assume data expires in 1 minutes
   }
+  uR.storage.times = uR.storage.get("times") || {};
 })();
