6f3028bafd59c025f40c08b6568035751986402a
chriscauley <chris@lablackey.com>
2017-07-14 02:55:36 -0400

    time demo and adding tomorrow

--- time.js
+++ time.js
@@ -1,20 +1,26 @@
 uR.ready(function() {
-  function _t(func) {
-    return function() {
-      return func(window.moment(new Date(this)));
-    }
+  String.prototype.moment = function moment() {
+    var m = window.moment(new Date(this));
+    return m
   }
-  String.prototype.date = _t(
-    function date(moment) { return moment.format("YYYY-MM-DD") }
-  )
-  String.prototype.verbose_date = _t(function (m){
-    var now = moment();
-    if (Math.abs(m-now) < 1000 * 3600 * 24 *60) { // less than two months away from now
+  String.prototype.date = function() { return  this.moment().format("YYYY-MM-DD"); }
+  String.prototype.hdate = function (){
+    var now = window.moment();
+    var m = this.moment();
+    var diff = m-now;
+    if (Math.abs(diff) < 1000 * 3600 * 24 *60) { // less than two months away from now
       if (now.month() == m.month() && now.date() == m.date()) {
-        return "Today"
+        return "Today";
+      }
+      if (diff > 0) {
+        if (diff < 1000*3600*24) { return "Tomorrow" }
       }
       return m.format("MMMM Do");
     }
     return m.format("MMMM Do, YYYY")
-  })
+  }
+  String.prototype.htime= function() {
+    var m  = this.moment();
+    return m.format('h? A').replace("?",m.minutes()?m.format(":mm"):"")
+  }
 })
