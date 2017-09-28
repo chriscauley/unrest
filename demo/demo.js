7fac9ed58971bacf053cfc09be01907b8b9c93b7
chriscauley <chris@lablackey.com>
2017-09-28 12:03:35 -0400

    refactoring demo to use external templates

--- demo/demo.js
+++ demo/demo.js
@@ -1,26 +1,36 @@
-var request = new Http.Get("http://holidayapi.com/v1/holidays?country=US&year=2015&api_key=444aa655-ae73-44b7-9ecf-3e29a37dfbb6",true);
-request.start().then( function(response) {
-  var holidays = JSON.parse(response).holidays;
-  var dates = []
-  for (var date in holidays) {
-    dates.push(date)
-  }
-  occurrences = [
-    { 'start': '2015-6-20 7:00 pm', 'name': 'My birthday party', 'end': '2015-6-20 9:00 pm' }
-  ];
-  for (var i=0;i<dates.length;i++) {
-    var d = dates[i];
-    for (var i2=0;i2<holidays[d].length;i2++) {
-      var o = holidays[d][i2];
-      o.start = o.date;
-      occurrences.push(o);
+uR.ready(function() {
+  uR.config.MODAL_PREFIX = "#!"; // this app uses has routing
+  uR.config.STATIC_URL = "/demo/";
+  uR.config.default_tabs = true;
+  uR.startRouter();
+  uR.addRoutes({
+    "#tabs-demo": function() { uR.loadTemplate("tabs-demo") },
+    "#markdown-demo": function() { uR.loadTemplate("markdown-demo") },
+    "#calendar-demo": function() { uR.loadTemplate("calendar-demo") }
+  });
+  uR.ajax({
+    url:"holidays.json",
+    success: function(data) {
+      var dates = []
+      for (var date in data) { dates.push(date) }
+      occurrences = [
+        { 'start': '2015-6-20 7:00 pm', 'name': 'My birthday party', 'end': '2015-6-20 9:00 pm' }
+      ];
+      for (var i=0;i<dates.length;i++) {
+        var d = dates[i];
+        for (var i2=0;i2<data[d].length;i2++) {
+          var o = data[d][i2];
+          o.start = o.date;
+          occurrences.push(o);
+        }
+      }
+      riot.mount("calendar",{occurrences: occurrences});
     }
-  }
-  riot.mount("calendar",{occurrences: occurrences});
-});
+  });
+  riot.mount("markdown,ur-tabs");
+})
 
 function foo(e) {
   alert("foo "+e);
 }
 
-riot.mount("markdown,ur-tabs");
