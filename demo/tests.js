6d13939987a053728c0a2ba75cb72fa77580a27e
chriscauley <chris@lablackey.com>
2018-02-11 19:29:39 -0500

    tests for new storage features

--- demo/tests.js
+++ demo/tests.js
@@ -1,3 +1,48 @@
+function testStorage() {
+  this.do()
+    .then(function checkSet() {
+      uR.storage.set("setKey",1);
+      uR.storage.set("removedKey",1);
+      uR.storage.remove("removedKey");
+      return uR.storage.get("setKey") == 1 && !uR.storage.has("removedKey")
+    })
+    .reloadWindow()
+    .then(function checkStorage() {
+      return uR.storage.get("setKey") == 1
+    })
+    .then(function checkRemove() {
+      return !uR.storage.has("removedKey")
+    })
+}
+
+function testStorageDefault() {
+  var store = new uR.Storage("TestStorageDefault");
+  store.getDefault("test",1)
+  store.getDefault("test2",2,"integer")
+  store.getDefault("test2",3) // doesn't override
+  store.getDefault("default1now3",1)
+  function allValid() {
+    return store.get("test") == 1 && store.get("test2") == 2 && store.get('default1now3') == 3;
+  }
+  this.do()
+    .then(function storeValuesInStorage() {
+      store.set('default1now3',3); // override
+      return true;
+    })
+    .then(allValid)
+    .reloadWindow()
+    .then(allValid)
+    .checkResults(function getData() { return store.getData() })
+    .checkResults(function getSchema() { return store.getSchema() })
+    .checkResults(function setSchema() {
+      return store.setSchema([
+        {name: "A", _default: 'a'},
+        {name: "OneIsInt", _default: 1, type: 'integer'}
+      ]);
+    })
+}
+
+
 function testForm() {
   this.do("ur-form tests")
     .setPath("#ur-form-demo")
@@ -42,4 +87,6 @@ function testCalendar() {
     .done("calendar has not changed")
 }
 
-konsole.addCommands(testForm,testTabs,testMarkdown,testCalendar);
+konsole.addCommands(
+  testStorage,testStorageDefault,
+  testForm,testTabs,testMarkdown,testCalendar);
