8d14420f82a7cc6f11adc7efe7c266b57dee7949
chriscauley <chris@lablackey.com>
2018-02-24 09:08:07 -0500

    editiable and toSchema

--- db/fields/abstract.js
+++ db/fields/abstract.js
@@ -1,13 +1,22 @@
 uR.db.BaseField = class BaseField extends uR.Object {
   constructor(opts) {
     super();
-    this.defaults(opts,{});
+    this.defaults(opts,{
+      editable: uR.DEFAULT_TRUE,
+    });
     var parent = this.parent || {};
     this.required = this.required == undefined || this.required; // defaults to true!
 
     this.name = this.name || this.type;
     this.value = this.value || (parent.initial || {})[this.name] || (parent.options || {})[this.name];
     this.initial_value = this.value;
+  }
+  toSchema() {
+    return {
+      name: this.name,
+      type: this.type,
+      value: this.value,
+    }
   }
 }
 
