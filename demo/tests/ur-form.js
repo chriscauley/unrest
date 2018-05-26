f739c6d622508f3e7ea39d995371fd9f5bf2dd2a
chriscauley <chris@lablackey.com>
2018-05-26 02:33:41 +0000

    adding select, checkbo, and radio to basic test form

--- demo/tests/ur-form.js
+++ demo/tests/ur-form.js
@@ -7,12 +7,25 @@ function testForm() {
     .changeValue("#id_last_name","Larou")
     .changeValue("#id_email","testy@example.com")
     .changeValue("#id_phone_number","541-908-0704")
+    .click("#id_colors__1")
+    .click("#id_colors__2")
+    .changeValue("#id_favorite_color","red")
+    .click("#id_radio_color__0")
     .checkResults("ur-form-demo ur-form")
     .checkResults(function getData() { return uR.form.current.getData() })
     .changeValue("#id_first_name","")
     .changeValue("#id_last_name","")
     .changeValue("#id_email","")
     .changeValue("#id_phone_number","")
+    .click("#id_colors__1")
+    .click("#id_colors__2")
+    .then(function() {
+      var e = document.querySelector("#id_radio_color__0");
+      e.checked=false;
+      e.dispatchEvent(new Event("change"));
+      return true;
+    })
+    .changeValue("#id_favorite_color","")
     .checkResults("ur-form-demo ur-form")
     .done("ur-form is great!")
 }
