2eb0d20d3d0b8665ecf175ccdf12d8c8faf008d8
chriscauley <chris@lablackey.com>
2018-05-28 20:04:48 +0000

    moving watchTimers interval funciton out and then calling said function immediately, as well as on an interval

--- lunchtime/lunchtime.js
+++ lunchtime/lunchtime.js
@@ -137,24 +137,26 @@
 
   var tt_cache = {};
   var target_time_interval;
+  function _watch() {
+    var elements = document.querySelectorAll("[data-target_time]");
+    var now = new Date().valueOf();
+    // var count = 0 // #! TODO Revisit how frequently this is called... could become a performance issue
+    for (var e of elements) {
+      var target = e.dataset.target_time;
+      if (e.textContent && tt_cache[target] && tt_cache[target] > now) { return }
+      //count ++; // #! ibid
+      delta_ms = now-target;
+      e.textContent = String.lunch.ms2hdelta(delta_ms);
+      tt_cache[target] = now;
+      if (delta_ms < 3600000) { return } // less than an hour needs immediate update
+      tt_cache[target] += 60000; // update in a minute
+    };
+    //console.log(count) #! ibid
+  }
   Sl.watchTimers = function() {
     clearInterval(target_time_interval);
-    target_time_interval = setInterval(function() {
-      var elements = document.querySelectorAll("[data-target_time]");
-      var now = new Date().valueOf();
-      // var count = 0 // #! TODO Revisit how frequently this is called... could become a performance issue
-      uR.forEach(elements,function(e,i) {
-        var target = e.dataset.target_time;
-        if (tt_cache[target] && tt_cache[target] > now) { return }
-        //count ++; // #! ibid
-        delta_ms = now-target;
-        e.textContent = String.lunch.ms2hdelta(delta_ms);
-        tt_cache[target] = now;
-        if (delta_ms < 3600000) { return } // less than an hour needs immediate update
-        tt_cache[target] += 60000; // update in a minute
-      });
-      //console.log(count) #! ibid
-    },1000);
+    _watch()
+    target_time_interval = setInterval(_watch,1000);
   }
   Sl.stopTimers = function() { clearInterval(target_time_interval); }
 })();
