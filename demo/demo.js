0255627bb66d2b419b7de395a2c8082aa1c610be
chriscauley <chris@lablackey.com>
2018-08-05 01:41:32 +0000

    tests were loading twice on route

--- demo/demo.js
+++ demo/demo.js
@@ -1,14 +1,18 @@
 uR.ready(function() {
-  uR.config.STATIC_URL = "/demo/";
-  uR.config.default_tabs = true;
-  uR.router.start();
+  uR.config.STATIC_URL = "/demo/"
+  uR.config.default_tabs = true
+  uR.router.start()
   uR.router.add({
     "#([^/]+)-demo": function(path,data) {
-      uR.loadTemplate(data.matches[1]+"-demo",data);
-      uR.newElement("script",{
-        src: "tests/"+data.matches[1]+".js",
-        parent: document.body,
-      });
+      const name = data.matches[1]
+      uR.loadTemplate(name+"-demo",data)
+      if (!document.getElementById(name+"__tests")) {
+        uR.newElement("script",{
+          id: name+"__tests",
+          src: "tests/"+name+".js",
+          parent: document.body,
+        })
+      }
     },
   });
 })
