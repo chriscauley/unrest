0da522eab98900b45ac15984b14fbb467729d358
chriscauley <chris@lablackey.com>
2018-08-31 17:29:58 +0000

    cleaning up serialize form function, adding dummy FIELD__{$i} when field has no name

--- src/js/index.js
+++ src/js/index.js
@@ -54,21 +54,22 @@
   uR.escapeHTML = function escapeHTML(s) { // taken from under-construction/lib/diff.js
     return s && s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
   }
-  uR.serialize = function serialize(form) {
+  uR.serializeForm = function serializeForm(form) {
     var field, s = [];
     if (typeof form != 'object' && form.nodeName != "FORM") { return }
     var len = form.elements.length;
     for (var i=0; i<len; i++) {
       field = form.elements[i];
-      if (!field.name || field.disabled || field.type == 'file' || field.type == 'reset' ||
-          field.type == 'submit' || field.type == 'button') { continue }
-      if (field.type == 'select-multiple') {
+      const name = field.name || "FIELD__"+i
+      const type = field.type
+      if (field.disabled || type == 'file' || type == 'reset' || type == 'submit' || type == 'button') { continue }
+      if (type == 'select-multiple') {
         for (j=form.elements[i].options.length-1; j>=0; j--) {
           if(field.options[j].selected)
-            s[s.length] = encodeURIComponent(field.name) + "=" + encodeURIComponent(field.options[j].value);
+            s.push(encodeURIComponent(name) + "=" + encodeURIComponent(field.options[j].value))
         }
-      } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
-        s[s.length] = encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value);
+      } else if ((type != 'checkbox' && type != 'radio') || field.checked) {
+        s.push(encodeURIComponent(name) + "=" + encodeURIComponent(field.value))
       }
     }
     return s.join('&').replace(/%20/g, '+');
