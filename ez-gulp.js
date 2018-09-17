98f883d5c5f0c6ec67d79d113bbd3c5d392b3eaf
chriscauley <chris@lablackey.com>
2018-09-17 19:20:28 +0000

    cleaning up ez-gulp a bit and adding capability to have keys specify .dist path like "demo/tests"

--- ez-gulp.js
+++ ez-gulp.js
@@ -19,60 +19,57 @@ module.exports = function(opts) {
   opts.static = opts.static || []; // files to be directly copied over
   opts.mustache = opts.mustache || []; // mustache templates to be processed
   var build_tasks = ['cp-static'];
-  var results = []
-  for (var key in opts.js) {
-    (function(key) {
-      results.push(key + '-built.js');
-      build_tasks.push("build-"+key);
-      gulp.task('build-'+key, function () {
-        if (key == "vendor") { // vendor files are already minified and babel-ifiied
-          return gulp.src(opts.js[key])
-            .pipe(concat(key + '-built.js'))
-            .pipe(gulp.dest(opts.DEST));
-        }
-        return gulp.src(opts.js[key])
-          .pipe(sourcemaps.init())
-          .pipe(riot())
-          .pipe(babel({
-            presets: [ 'es2017' ],
-            plugins: [ "transform-object-rest-spread" ],
-          }))
-          .pipe(concat(key + '-built.js'))
-          .pipe(sourcemaps.write("."))
-          .pipe(gulp.dest(opts.DEST))
-      });
-    })(key)
+  var results = [];
+  for (let key in opts.js) {
+    results.push(key + '-built.js');
+    build_tasks.push("build-"+key);
+    const dirs = key.split("/");
+    const name = dirs.pop();
+    const dest = path.normalize(opts.DEST+"/"+dirs.join("/"));
+    gulp.task('build-'+key, function () {
+      if (key == "vendor") { // vendor files are already minified and babel-ifiied
+        return gulp.src(opts.js[key]).pipe(concat(key + '-built.js')).pipe(gulp.dest(dest));
+      }
+      return gulp.src(opts.js[key])
+        .pipe(sourcemaps.init())
+        .pipe(riot())
+        .pipe(babel({
+          presets: [ 'es2017' ],
+          plugins: [ "transform-object-rest-spread" ],
+        }))
+        .pipe(concat(name + '-built.js'))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(dest))
+    });
   }
 
-  for (var key in opts.less) {
-    (function(key) {
-      build_tasks.push("build-"+key+"-css");
-      results.push(key+'-built.css')
-      gulp.task("build-"+key+"-css", function () {
-        return gulp.src(opts.less[key])
-          .pipe(less({}))
-          .pipe(concat(key+'-built.css'))
-          .pipe(gulp.dest(opts.DEST));
-      });
-    })(key);
+  for (let key in opts.less) {
+    build_tasks.push("build-"+key+"-css");
+    results.push(key+'-built.css')
+    gulp.task("build-"+key+"-css", function () {
+      return gulp.src(opts.less[key])
+        .pipe(less({}))
+        .pipe(concat(key+'-built.css'))
+        .pipe(gulp.dest(opts.DEST));
+    });
   }
 
-  gulp.task('clean',function() {
+  gulp.task('clean', function() {
     return gulp.src(opts.DEST+"/*",{read: false})
       .pipe(clean())
   })
 
   gulp.task("cp-static",function() {
-    var DEST = opts.DEST;
+    let DEST = opts.DEST;
     if (!DEST.startsWith("/")) {
       DEST = path.join(__dirname,opts.DEST);
     }
     opts.static.forEach(function(file_or_directory) {
-      var source = path.join(__dirname,file_or_directory);
+      let source = path.join(__dirname,file_or_directory);
       var dest = path.join(DEST,file_or_directory.replace(/src\//,''));
       ncp(source, dest);
     });
-    opts.renames && opts.renames.forEach(function(r) {
+    (opts.renames || []).forEach((r) => {
       ncp(path.join(__dirname,r[0]),path.join(DEST,r[1]));
     });
   });
@@ -113,7 +110,8 @@ module.exports = function(opts) {
     build_tasks.push('build-mustache');
     watch_tasks.push('build-mustache');
   }
-  gulp.task('watch', build_tasks, function () {
+
+  gulp.task('watch', ['default'], function() {
     for (var key in opts.js) {
       gulp.watch(opts.js[key], ['build-'+key].concat(watch_tasks))
     }
@@ -124,6 +122,6 @@ module.exports = function(opts) {
     gulp.watch(opts.static.map(d => d.replace(/\/$/,"/**")),['cp-static']);
   });
 
-  gulp.task('default', build_tasks);
+  gulp.task('default', build_tasks)
   gulp.task('deploy', build_tasks);
 }
