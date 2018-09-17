10a1e72446891d848333afd246f0e6d064634296
chriscauley <chris@lablackey.com>
2018-09-17 20:35:16 +0000

    more calculator tests

--- demo/tests/calculator.js
+++ demo/tests/calculator.js
@@ -13,4 +13,7 @@ function testBtns(name,keys_array) {
 
 konsole.addCommands(
   testBtns("Addition",["1+1","2+5","3.999+1","8+0"]),
+  testBtns("Subtraction",["1-1","100-0.001","-1-1","-1--1"]),
+  testBtns("Multiplication",["0*5","1*5","1000*1000","0.5*0.6","0.5*0.000005","3*-4"]),
+  testBtns("Division",["1/2","10/2","8/1000000","1/0"]),
 )
