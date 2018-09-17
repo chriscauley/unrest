fef683ef439031571db75672746a42b2d7446ef9
chriscauley <chris@lablackey.com>
2018-09-17 21:36:47 +0000

    markdown test is (mostly) working

--- demo/tests/markdown.js
+++ demo/tests/markdown.js
@@ -1,6 +1,11 @@
 function testMarkdown() {
   this.do("testing markdown")
+    .route("#markdown-demo")
     .wait("#content markdown")
+    .checkResults()
+    .wait("ur-tab[title=Code]")
+    .checkResults()
+    .wait("ur-tab[title=Result]")
     .checkResults()
     .done("markdown has not changed")
 }
